e1e5b4ac58ef35f4bb2cceeaf1196c566a11ac2d
Daniel Chen <danielchenphhs@gmail.com>
2016-11-14 22:15:30 -0500

    Tweaked footprints

--- js/vis-footprint.js
+++ js/vis-footprint.js
@@ -3,7 +3,7 @@
 
 var margin = {top: 50, right: 50, bottom: 50, left: 50};
 
-var width = 1000 - margin.left - margin.right,
+var width = 800 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
 
 var svg = d3.select("#footprintvis").append("svg")
@@ -16,13 +16,13 @@ var x = d3.scale.linear()
     .range([width, 0]);
 
 var y = d3.scale.linear()
-    .range([height, 0]);
+    .range([0, height]);
 
 x.domain([0,100]);
 y.domain([0,100]);
 
 var r = d3.scale.linear()
-    .range([0,30]);
+    .range([5,30]);
 
 loadData();
 
@@ -63,6 +63,35 @@ function updateVisualization() {
     circs.enter()
         .append("circle");
 
+
+
+    var answer = 0;
+    var prev = 0;
+    var answers = [];
+    // svg3.selectAll(".legend-circ")
+    //     .data(sizes)
+    //     .enter()
+    //     .append("circle")
+    //     .attr("class","legend-circ")
+    //     .attr("fill", "#bdbdbd")
+    //     .attr("r", function(d) {
+    //         return d.size;
+    //     })
+    //     .attr("cy", function(d, index) {
+    //         answer = (d.size + answer + prev + 5);
+    //         prev = d.size;
+    //         answers[index] = answer + 40;
+    //         return answer + 40;
+    //     } )
+    //     .attr("cx", width3/2);
+
+
+    var tip = d3.tip().attr('class', 'd3-tip').offset([-10,0]).html(function(d) {
+        return d.country + " " + d.footprintallGHGpercapita;
+    });
+
+    svg.call(tip);
+
     circs.attr("class", "country-circ")
         .attr("fill", function(d) {
             return "blue";
@@ -71,9 +100,17 @@ function updateVisualization() {
             return x(index * 9);
         })
         .attr("cy", function(d,index) {
-            return y(index * 9);
+            answer = (d.footprintallGHGpercapita + answer + prev + 5);
+            prev = d.footprintallGHGpercapita;
+            answers[index] = answer + 40;
+            return answer + 40;
         })
-        .attr("r", function(d) {return r(d.footprintallGHGpercapita)});
+        .attr("r", function(d) {return r(d.footprintallGHGpercapita)})
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide); // Population;
+
+
+
 
     // Exit
     circs.exit().remove();
