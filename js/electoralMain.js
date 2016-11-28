268ad87bd72a8fb50a8e7f7a51e484930f0d6393
Daniel Chen <danielchenphhs@gmail.com>
2016-11-27 23:45:00 -0500

    electoral map senators

---
+++ js/electoralMain.js
@@ -0,0 +1,81 @@
+var allData = [];
+var stateTotalData;
+var senData;
+
+// Variable for the visualization instance
+var electoralMap;
+var congressVis;
+
+var dateFormatter = d3.time.format("%d-%b-%Y");
+
+// Start application by loading the data
+loadData();
+
+function loadData() {
+
+
+    // $.getJSON(proxy+url, function(jsonData){
+    //
+    //     data = jsonData.station;
+    //     data.forEach(function(d){
+    //         d.id = +d.id;
+    //         d.lat = +d.lat;
+    //         d.long = +d.long;
+    //         // d.nbBikes = +d.nbBikes;
+    //         // d.nbEmptyDocks = +d.nbEmptyDocks;
+    //     });
+    //
+    //     $("#station-count").text(data.length);
+    //
+    //
+    // });
+    queue()
+        .defer(d3.csv, "data/electoral/stateDeniersTotal.csv")
+        .defer(d3.csv, "data/electoral/senators.csv")
+        .await(function(error, totalCSV, senCSV) {
+
+            totalCSV.forEach(function(d) {
+                d.Population = +d.Population;
+                d.repDeniers114 = +d.repDeniers114;
+                d.repTotal114 = +d.repTotal114;
+                d.senDeniers = +d.senDeniers;
+                d.senTotal = +d.senTotal;
+            });
+
+            senCSV = senCSV.filter(function(d) {
+                return d['115Member'] == 1;
+            });
+
+
+            senCSV.forEach(function(d) {
+                d.AgeAtTakingOfficeDay = +d.AgeAtTakingOfficeDay;
+                d.AgeAtTakingOfficeYear = +d.AgeAtTakingOfficeYear;
+                d.BirthDate = dateFormatter.parse(d.BirthDate);
+                d.DateInOffice = dateFormatter.parse(d.DateInOffice);
+                d.YearNextElection = +d.YearNextElection;
+                d.YearsInOffice = +d.YearsInOffice;
+            });
+            // console.log(totalCSV);
+            // console.log(senCSV);
+
+            stateTotalData = totalCSV;
+            senData = senCSV;
+            createVis();
+        });
+
+}
+
+
+function createVis() {
+
+    var EventHandler = {};
+    electoralMap = new ElectoralMap("#electoral-map",stateTotalData,EventHandler);
+    congressVis = new CongressVis("#congress-vis",senData);
+    // $(EventHandler).bind("selectionChanged", function(event, rangeStart, rangeEnd){
+        // $("#startDate").text(dateFormatter(rangeStart));
+        // $("#endDate").text(dateFormatter(rangeEnd));
+        // ageVis.onSelectionChange(rangeStart, rangeEnd);
+        // prioVis.onSelectionChange(rangeStart, rangeEnd);
+    // });
+
+}
---
+++ js/electoralMap.js
@@ -0,0 +1,81 @@
+/*
+ *  ElectoralMap - Object constructor function
+ *  @param _parentElement   -- HTML element in which to draw the visualization
+ *  @param _data            -- Array with all stations of the bike-sharing network
+ */
+
+ElectoralMap = function(_parentElement, _data, _eventHandler) {
+
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.eventHandler = _eventHandler;
+
+    this.initVis();
+};
+
+ElectoralMap.prototype.initVis = function() {
+
+    var vis = this;
+
+    vis.width = 1000;
+
+    vis.height = 600;
+
+    vis.svg = d3.select(vis.parentElement).append("svg")
+        .attr("width", vis.width)
+        .attr("height", vis.height);
+
+    d3.json("data/us-states.json", function(json) {
+        vis.json = json;
+        vis.wrangleData();
+    });
+};
+
+ElectoralMap.prototype.wrangleData = function() {
+    var vis = this;
+
+    for (var i = 0; i < vis.data.length; i++) {
+
+        console.log(vis.data[i].State)
+        for (var j = 0; j < vis.json.features.length; j++) {
+            if (vis.json.features[j].properties.name == vis.data[i].State) {
+                vis.json.features[j].properties.senDeniers = vis.data[i].senDeniers;
+                break;
+            }
+        }
+    }
+
+    // console.log(vis.json)
+
+
+    // vis.displayData = vis.data;
+
+    // Update the visualization
+    vis.updateVis();
+
+};
+
+ElectoralMap.prototype.updateVis = function() {
+    var vis = this;
+
+    vis.projection = d3.geo.albersUsa()
+        .translate([vis.width / 2, vis.height / 2])
+        .scale([1000]);
+
+    vis.path = d3.geo.path()
+        .projection(vis.projection);
+
+    // Render the U.S. by using the path generator
+    vis.svg.selectAll("path")
+        .data(vis.json.features)
+        .enter().append("path")
+        .attr("d", vis.path)
+        .attr("stroke","white")
+        .attr("fill",function(d) {
+            if (d.properties.senDeniers == 2) {return "red"}
+            else if (d.properties.senDeniers == 1) {return "purple"}
+            else {return "blue"}
+        })
+        .on("mouseover",function(d) { console.log (d.properties.senDeniers)});
+
+};
