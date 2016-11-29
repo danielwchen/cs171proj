67ad16124e089c6ca7cd56d52d507f43863f4669
zbathen <zbathen@gmail.com>
2016-11-28 20:37:36 -0500

    updated footprints

--- js/vis-footprint.js
+++ js/vis-footprint.js
@@ -3,26 +3,23 @@
 
 var margin = {top: 50, right: 50, bottom: 50, left: 50};
 
-var width = 800 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+var width = 900 - margin.left - margin.right,
+    height = 5900 - margin.top - margin.bottom;
 
-var svg = d3.select("#footprintvis").append("svg")
+var svg = d3.select("#footprint-vis").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scale.linear()
-    .range([width, 0]);
-
-var y = d3.scale.linear()
-    .range([0, height]);
-
-x.domain([0,100]);
-y.domain([0,100]);
+var svgSide = d3.select("#avg-footprint").append("svg")
+    .attr("width", 200)
+    .attr("height", 200);
 
 var r = d3.scale.linear()
-    .range([5,30]);
+    .range([1,200]);
+
+var yPlaceholder = 0;
 
 loadData();
 
@@ -31,12 +28,11 @@ var data;
 // Load CSV file
 function loadData() {
 
-    d3.csv("data/footprint.csv", function(error, csv) {
+    d3.csv("data/carbonByCountry.csv", function(error, csv) {
 
         csv.forEach(function(d){
-            d.footprintallGHGpercapita = +d.footprintallGHGpercapita;
-            d.CH4percapita = +d.CH4percapita;
-            d.footprintallGHG = +d.footprintallGHG;
+            d.Total = +d.Total;
+            d.PC = +d.PC;
         });
 
         data = csv;
@@ -50,74 +46,90 @@ function loadData() {
 function updateVisualization() {
 
     data = data.sort(function(a,b) {
-        return b.footprintallGHGpercapita - a.footprintallGHGpercapita;
+        return getData(b) - getData(a);
     });
 
 
-    r.domain(d3.extent(data, function(d) { return d.footprintallGHGpercapita; }));
 
+    yPlaceholder = 0;
+
+    console.log(data);
+
+    r.domain(d3.extent(data, function(d){return getData(d);}));
 
     var circs = svg.selectAll("circle")
+        .data(data);
+
+    var markers = svg.selectAll("path")
         .data(data);
 
     circs.enter()
         .append("circle");
 
-
-
-    var answer = 0;
-    var prev = 0;
-    var answers = [];
-    // svg3.selectAll(".legend-circ")
-    //     .data(sizes)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("class","legend-circ")
-    //     .attr("fill", "#bdbdbd")
-    //     .attr("r", function(d) {
-    //         return d.size;
-    //     })
-    //     .attr("cy", function(d, index) {
-    //         answer = (d.size + answer + prev + 5);
-    //         prev = d.size;
-    //         answers[index] = answer + 40;
-    //         return answer + 40;
-    //     } )
-    //     .attr("cx", width3/2);
-
+    markers.enter()
+        .append("path");
 
     var tip = d3.tip().attr('class', 'd3-tip').offset([-10,0]).html(function(d) {
-        return d.country + " " + d.footprintallGHGpercapita;
+        return d.Country + " " + getData(d);
     });
 
     svg.call(tip);
 
-    circs.attr("class", "country-circ")
+    circs
+        .attr("class", "country-circ")
         .attr("fill", function(d) {
             return "blue";
         })
-        .attr("cx", function(d,index) {
-            return x(index * 9);
+        .attr("cx", function(d) {
+            return width - 200;
         })
-        .attr("cy", function(d,index) {
-            answer = (d.footprintallGHGpercapita + answer + prev + 5);
-            prev = d.footprintallGHGpercapita;
-            answers[index] = answer + 40;
-            return answer + 40;
+        .attr("cy", function(d) {
+            d.yVal = height - yPlaceholder - 20 - r(getData(d));
+            yPlaceholder = yPlaceholder + 20 + 2*r(getData(d));
+            return d.yVal;
         })
-        .attr("r", function(d) {return r(d.footprintallGHGpercapita)})
+        .attr("r", function(d) {return r(getData(d))})
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide); // Population;
 
+    markers
+        .attr("d", function(d, index){
+            // Shift the triangles on the x-axis (columns)
+            var x = width - 250 - r(getData(d));
 
+            // All triangles of the same row have the same y-coordinates
+            // Vertical shifting is already done by transforming the group elements
+            var y = d.yVal - 20;
+
+            return 'M ' + x + ' ' + y + ' l 50 20 l -50 20 z';
+        })
+        .attr("fill", function(d){
+            switch(d.Country){
+                case "Nauru":
+                    return "green";
+                case "Nicaragua":
+                    return "lime";
+                case "United Kingdom":
+                    return "yellow";
+                case "India":
+                    return "orange";
+                case "United States":
+                    return "red";
+                case "China":
+                    return "red";
+                default:
+                    return "none";
+            }
+
+        });
 
 
     // Exit
     circs.exit().remove();
-    console.log("check")
+    markers.exit().remove();
 }
-//
-// d3.select(".form-control")
-//     .on("change", function() {
-//         updateVisualization();
-//     });
+
+function getData(country){
+    var input = $('input[name="dataS"]:checked').val();
+    return (input == "total") ? country.Total : country.PC;
+}
