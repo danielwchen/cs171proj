8793f36f9467748bbd05340ffc2e115134891f80
Grace <glin@college.harvard.edu>
2016-12-09 15:48:48 -0500

    prelim extremetemp viz

--- js/extremeTemperaturesMain.js
+++ js/extremeTemperaturesMain.js
@@ -2,30 +2,30 @@ var recordsData;
 var recordMap;
 
 
-var yearFormatter = d3.time.format("%Y");
-var monthFormatter = d3.time.format("%m");
+// var yearFormatter = d3.time.format("%Y");
+// var monthFormatter = d3.time.format("%m");
 
 loadData();
 
 
 
 function loadData(){
-	queue()
+        queue()
         .defer(d3.csv, "data/allCityRecords.csv")
         .await(function(error, tempRecordsCSV) {
-        	tempRecordsCSV.forEach(function(d){
-        		d.Year = yearFormatter.parse(d.Year);
-        		d.Month = monthFormatter.parse(d.Month);
-        		d.Temperature = +d.Temperature;
-        	});
+                tempRecordsCSV.forEach(function(d){
+                        // d.Year = yearFormatter.parse(d.Year);
+                        // d.Month = monthFormatter.parse(d.Month);
+                        d.Temperature = +d.Temperature;
+                });
 
 
-        	recordsData = tempRecordsCSV;
-        	createVis();
+                recordsData = tempRecordsCSV;
+                createVis();
         });
 }
 
 
 function createVis() {
-	recordMap = new RecordMap("#extremeTempVis", recordsData);
+        recordMap = new RecordMap("#extremeTempVis", recordsData);
 }
--- js/recordMap.js
+++ js/recordMap.js
@@ -1,18 +1,9 @@
 
 
-// var projection = d3.geo.albersUSA();
 
-
-// var path = d3.geo.path()
-//     .projection(projection);
-
-// svg.selectAll("path").append("path")
-//     .attr("d", path)
-//     .attr("fill", "steelblue")
 
 
 RecordMap = function(_parentElement, _data) {
-
     this.parentElement = _parentElement;
     this.data = _data;
 
@@ -22,14 +13,58 @@ RecordMap = function(_parentElement, _data) {
 RecordMap.prototype.initVis = function() {
 
     var vis = this;
-    vis.width = 800;
+    vis.width = 900;
 	vis.height = 600;
-
+    vis.filteredData = vis.data;
 
 	vis.svg = d3.select(vis.parentElement).append("svg")
 		.attr("width", vis.width)
 		.attr("height", vis.height);
 
-	console.log("testing")
 
+    vis.projection = d3.geo.albersUsa();
+
+    vis.path = d3.geo.path()
+        .projection(vis.projection);
+
+
+    vis.year = 2001;
+
+    d3.json("data/us-states.json", function(json) {
+        vis.json = json;
+        vis.svg.selectAll(".state-border")
+            .data(vis.json.features)
+            .enter().append("path")
+            .attr("class", "state-border")
+            .attr("d", vis.path)
+            .attr("fill", "yellowgreen")
+            .style("stroke-width", "2px");
+
+
+        vis.wrangleData();
+    });
+
+}
+
+RecordMap.prototype.wrangleData = function(){
+    var vis = this;
+    vis.filteredData = vis.data.filter(function(d, index){
+        return d.Year == vis.year;
+    });
+    console.log(vis.filteredData);
+
+    vis.updateVis();
+}
+
+RecordMap.prototype.updateVis = function(){
+    var vis = this;
+    vis.svg.selectAll(".records")
+        .data(vis.filteredData)
+        .enter().append("circle")
+        .attr("class", "records")
+        .attr("r", 3)
+        .attr("fill", "navy")
+        .attr("transform", function(d){
+            return "translate(" + vis.projection([-1 * (+d.Longitude.substring(0, 5)), +(d.Latitude.substring(0, 4))]) + ")";
+        });
 }
