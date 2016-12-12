c16e56bfd903807d07a85a3406a8fa75e2d0a808
zbathen <zbathen@gmail.com>
2016-12-12 01:28:00 -0500

    troubleshooting git

---
+++ js/homepage.js
@@ -0,0 +1,17 @@
+
+function introduction(){
+    var introText = [
+        "Sure, the graph is true, but it starts to lose it's power after a while...",
+        "Climate change has NOT lost any power.",
+        "With each passing year we move closer to the edge of the cliff.",
+        "We can't afford to forget."
+        "That's why we decided to make something a bit more interesting..."
+        "Click the arrow on the right to learn more."
+    ];
+    counter = 0;
+
+    if (counter < introText.length){
+        document.getElementById("introduction").innerHTML = introText[counter];
+        counter += 1;
+    }
+}
