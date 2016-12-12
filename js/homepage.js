8aa1d43dcd9299811d570d62915f48d913910ace
zbathen <zbathen@gmail.com>
2016-12-12 01:28:30 -0500

    troubleshiooting git more

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,15 @@
 
+var counter = 0;
+
 function introduction(){
     var introText = [
-        "Sure, the graph is true, but it starts to lose it's power after a while...",
+        "Sure, the graph is true, but it starts to lose it's power after awhile... \n (click again)",
         "Climate change has NOT lost any power.",
         "With each passing year we move closer to the edge of the cliff.",
-        "We can't afford to forget."
-        "That's why we decided to make something a bit more interesting..."
+        "We can't afford to forget.",
+        "That's why we decided to make something a bit more interesting...",
         "Click the arrow on the right to learn more."
     ];
-    counter = 0;
 
     if (counter < introText.length){
         document.getElementById("introduction").innerHTML = introText[counter];
